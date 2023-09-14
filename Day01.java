9f376ad55c616b64b5aef3501e9ab69027c8e15c
amolpalwe2002 <amolpalwe2002@gmail.com>
2023-09-15 03:25:01 +0530

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,8 @@
+//Write java program to print "Day01 of Java Programming." on the terminal.
+
+class Day01{
+    public static void main(String[] args){
+        //print statement in java (System.out.println())
+        System.out.println("Day01 of Java Programming.");
+    }
+}
